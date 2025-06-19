2add9959c2f6cd39f25fb9e6ae3ccea791615b2b
Qurban <qurban@Qurbans-MacBook-Pro.local>
2025-06-19 15:26:45 +0400

    initial commit

---
+++ src/main/java/com/mercury/botcreator/BotCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.mercury.botcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotCreatorApplication.class, args);
+    }
+
+}
